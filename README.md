9ccb50be36fa207f388cd5ca178fa59529451149
BuildTools <david.glaude@gmail.com>
2016-08-12 23:00:39 +0200

    Refresh from local repository

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-PyTIX is an implementation of the TIX LED clock in Python. It uses [curses][1] to draw the clock to a terminal window.
+PyTIX is an implementation of the TIX LED clock in Python.
+
+The original version[1] used [curses][2] to draw the clock to a terminal window.
+This version use the mote[3] library to display the time.
+Require mote hardware from Pimoroni.
 
 ##Usage
 
-`python pytix_c.py [update interval, defaults to 4 seconds] ["--24" for 24-hour mode]`
+`python pytix_mote.py [update interval, defaults to 1 seconds] ["--12" for 12-hour mode]`
 
 ##Interpretation
 
@@ -12,4 +16,6 @@ Count the groups of colored squares from left to right to get the time&mdash;e.g
 
 `Ctrl-C` at the moment.
 
-[1]: http://docs.python.org/library/curses.html
+[1]: https://github.com/mdoege/PyTIX
+[2]: http://docs.python.org/library/curses.html
+[3]: https://github.com/pimoroni/mote
--- pytix_mote.py
+++ pytix_mote.py
@@ -3,19 +3,42 @@
 # Displays the TIX clock using Curses
 # 2011-01-08
 
-# Usage: pytix_c.py [update interval] [--24]
+# Modified to use mote by David Glaude
+# Modified for my prefered default value: 1 second update and 24 Hours
+# 2016-08-12
 
-import sys, time, random, curses
+# Usage: pytix_mote.py [update interval] [--12]
 
+import sys, time, random
+
+try:
+    from mote import Mote
+except ImportError:
+    exit("This script requires the mote module\nInstall with: pip install mote")
+
+## I don't know why a minimum version is required...
 assert sys.version >= '2.3', "Python 2.3 or later required."
 
 try: inter = int(sys.argv[1])
-except: inter = 4		# default update interval (secs)
+except: inter = 1		# default update interval (secs)
 
 if '--24' in sys.argv:
-	f = '%H%M'
-else:
 	f = '%I%M'
+else:
+	f = '%H%M'
+
+colors = [
+    (  0,   0,   0),  # Unused position in the colour table
+    (  0,   0,   0),  # 1 Colour for unused position between digit
+    (  1,   0,   0),  # 2 OFF Colour for hours first digit = RED
+    (255,   0,   0),  # 3 ON  Colour for hours first digit = RED
+    (  0,   1,   0),  # 4 OFF Colour for hours second digit = GREEN
+    (  0, 255,   0),  # 5 ON  Colour for hours second digit = GREEN
+    (  0,   0,   1),  # 6 OFF Colour for minutes first digit = BLUE
+    (  0,   0, 255),  # 7 ON  Colour for minutes first digit = BLUE
+    (  1,   0,   0),  # 8 OFF Colour for minutes first digit = BLUE
+    (255,   0,   0)   # 9 ON  Colour for minutes last digit = RED
+]
 
 def tog(start, end, n, col = 2):
 	"Toggle on n values randomly in the array between start and end."
@@ -24,44 +47,40 @@ def tog(start, end, n, col = 2):
 	for z in random.sample(range(3 * (end - start)), n):
 		disp[z % 3][start + z // 3] = col
 
-def getsym(n):
-	"Return the on/off LED symbol."
-	if n > 2: return ' '
-	elif n == 1: return '-'
-	else: return '+'
+def mainprog():
+	"Display current time with pixels on the mote."
+	global disp,mote
 
-def mainprog(win):
-	global disp
+	t = time.strftime(f, time.localtime())
+	h1, h2, m1, m2 = [int(x) for x in t]
+	disp = [[2, 1, 4, 4, 4, 1, 6, 6, 1, 8, 8, 8] for y in range(3)]
+	tog( 0,  1, h1, 3)
+	tog( 2,  5, h2, 5)
+	tog( 6,  8, m1, 7)
+	tog( 9, 12, m2, 9)
+	for x in range(12):
+		for y in range(3):
+			r, g, b = colors[ disp[y][x] ]
+			mote.set_pixel(y+1, x, r, g, b)
+	mote.show()
 
-	assert curses.has_colors(), "Your terminal does not support colors."
-	try: curses.curs_set(0)
-	except: pass
-	boxx, boxy = win.getmaxyx()[1] // 16, win.getmaxyx()[0] // 10
+mote = Mote()
 
-	curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-	curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
-	curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_GREEN)
-	curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLUE)
+num_pixels = 16
 
-	while True:
-		t = time.strftime(f, time.localtime())
-		h1, h2, m1, m2 = [int(x) for x in t]
-		disp = [[2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2] for y in range(3)]
-		tog(0, 1, h1, 3)
-		tog(2, 5, h2, 4)
-		tog(6, 8, m1, 5)
-		tog(9, 12, m2, 3)
-		for x in range(12):
-			for y in range(3):
-				for yy in range(boxy):
-					win.addstr(y * (boxy + 1) + yy + boxy,
-						x * (boxx + 1) + boxx,
-						boxx * getsym(disp[y][x]),
-						curses.color_pair(disp[y][x]))
-		win.move(0, 0)
-		win.refresh()
-		time.sleep(inter)
+mote.configure_channel(1, num_pixels, False)
+mote.configure_channel(2, num_pixels, False)
+mote.configure_channel(3, num_pixels, False)
+mote.clear()
+mote.show()
 
-curses.wrapper(mainprog)
+try:
+    while True:
+        mainprog()
+        time.sleep(inter)
+
+except KeyboardInterrupt:
+    mote.clear()
+    mote.show()
+    time.sleep(0.1)
 
